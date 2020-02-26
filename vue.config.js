978bb6cfcb905f709a287f030e0dce17a25f88a4
DavidKity <541981334@qq.com>
2020-02-26 23:58:23 +0800

    svg文件解析配置、图标

--- vue.config.js
+++ vue.config.js
@@ -10,19 +10,28 @@ module.exports = {
      *  webpack配置，see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
      */
     chainWebpack: (config) =>{
-
+        const svgRule = config.module.rule("svg");     
+        svgRule.uses.clear();     
+        svgRule
+        .use("svg-sprite-loader")
+        .loader("svg-sprite-loader")
+        .options({ 
+        symbolId: "icon-[name]",
+        include: ["./src/icons"] 
+        });
     },
     configureWebpack: (config) => {
          config.resolve = { // 配置解析别名
              extensions: ['.js','.json','.vue'],  //自动添加文件名后缀
              alias: {
+                'vue': 'vue/dist/vue.js',  //改变vue3.0的默认指向路径
                 '@': path.resolve(__dirname, './src'),
-                 'public': path.resolve(__dirname, './public'),
-                 'compoments': path.resolve(__dirname, './src/components'),
-                 'common': path.resolve(__dirname, './src/common'),
-                 'api': path.resolve(__dirname, './src/api'),
-                 'view': path.resolve(__dirname, './src/view'),
-                 'data': path.resolve(__dirname, './src/data')
+                'public': path.resolve(__dirname, './public'),
+                'compoments': path.resolve(__dirname, './src/components'),
+                'common': path.resolve(__dirname, './src/common'),
+                'api': path.resolve(__dirname, './src/api'),
+                'view': path.resolve(__dirname, './src/view'),
+                'data': path.resolve(__dirname, './src/data')
             }
        }
     },
